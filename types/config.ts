696fc9c6f1bc6698a7f56f7b9c0dedaf5f0ebc39
hua1995116 <461249104@qq.com>
2018-04-11 11:47:44 +0800

    '增加静态处理'

--- types/config.ts
+++ types/config.ts
@@ -1,6 +1,6 @@
 export interface config {
-    pid: string; // 项目上报id
-    url: string; // 请求url
+    client: string; // 项目上报id
+    imgUrl: string; // 请求url
     level: Number; // 等级
     repeat: Number; // 重复上报次数
     ignore: Array<RegExp | Function>; // 过滤条件
